f8eda2ec4c72d139b1324908180766dfe3cf5ee5
kavinrajsi <sikavinraj@gmail.com>
2026-05-28 20:11:26 +0530

    feat: initial Payload schema

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260528_144106 from './20260528_144106';
+
+export const migrations = [
+  {
+    up: migration_20260528_144106.up,
+    down: migration_20260528_144106.down,
+    name: '20260528_144106'
+  },
+];
